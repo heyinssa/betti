a4dc5f6c45a93259f1f9f4886a0b7f0732f0e2c9
huni-backend <petercha2000@gmail.com>
2021-10-25 15:48:31 +0000

    Refactor: Lint 및 prettier 적용중 - model 빠진거 다시 넣음

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -10,14 +10,14 @@ import VersionModel from './version/version.js';
 
 /**
  * Model
- * 
+ *
  * @note
  * DB table 과 1:1 관계로 DB query를 추상화
- * 
+ *
  * @role
  * DB 스키마 정의 및 필요시 table 생성
  * DB 와 정상적으로 통신하여 적절한 query 요청
- * 
+ *
  * @interface
  * PK 에 대한 아래의 인터페이스 제공
  *   - getByPK
@@ -26,7 +26,7 @@ import VersionModel from './version/version.js';
  *   - remove
  * FK 에 대한 아래의 인터페이스 제공
  *   - getByFK
- * 
+ *
  * @input PK 가 유효해야함
  * @return query 결과
  * @error query 실패시
