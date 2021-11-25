9fc312e0703c5fc0aea89b836c684ffa7de8e6d1
chichoon <chichoon.choi@gmail.com>
2021-11-25 22:14:36 +0900

    Feat: prettier, eslint 세팅

---
+++ frontend/src/Routes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const Rounter = () => {
+  return (
+    <Router>
+      <Switch>
+        {
+          //Pages
+        }
+      </Switch>
+    </Router>
+  );
+};
+
+export default Rounter;
