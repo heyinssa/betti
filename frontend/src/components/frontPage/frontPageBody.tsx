51c7c5ac4b9f041bc6bf57f478640eb552052eff
chichoon <chichoon.choi@gmail.com>
2021-11-29 12:45:37 +0900

    Feat: Frontpage 컴포넌트 추가

--- frontend/src/components/frontPage/frontPageBody.tsx
+++ frontend/src/components/frontPage/frontPageBody.tsx
@@ -5,7 +5,7 @@ const FrontPageBody = () => {
     <div className="front-page-body">
       <div className="front-page-items">
         <div className="front-page-text">오늘 하루 무료로 사용하세요</div>
-        <div className="front-page-google">(google login)</div>
+        <div className="front-page-google">(login)</div>
       </div>
     </div>
   );
---
+++ frontend/src/components/frontPage/index.tsx
@@ -0,0 +1,3 @@
+import FrontPage from "./frontPage";
+
+export default FrontPage;
