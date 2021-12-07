2888462d65bcbdbfd0124db406cb833346140e05
chichoon <chichoon.choi@gmail.com>
2021-12-07 14:46:36 +0900

    Feat: 로그인 페이지 컴포넌트 추가

---
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,13 @@
+import { LoginForms } from '../../components';
+
+const LoginPage = () => {
+  return (
+    <>
+      <div className="page-login">
+        <LoginForms />
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;
