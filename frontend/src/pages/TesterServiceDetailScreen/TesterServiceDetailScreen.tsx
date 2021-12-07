8f61eb1cbedbafdc1a074f30d329d1af47829f7f
chichoon <chichoon.choi@gmail.com>
2021-12-08 01:14:18 +0900

    Feat: 프로젝트 상세 페이지 구성 (임시)

---
+++ frontend/src/pages/TesterServiceDetailScreen/TesterServiceDetailScreen.tsx
@@ -0,0 +1,17 @@
+import {
+  MarkdownTextContainer,
+  ProjectDetailHeader,
+  ProjectImageRow,
+} from "../../components";
+
+const TesterServiceDetailScreen = () => {
+  return (
+    <>
+      <ProjectDetailHeader />
+      <ProjectImageRow />
+      <MarkdownTextContainer />
+    </>
+  );
+};
+
+export default TesterServiceDetailScreen;
