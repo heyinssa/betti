ce210220c2628304e39604ab202dd8491cd4cc26
chichoon <chichoon.choi@gmail.com>
2021-12-07 17:25:05 +0900

    Feat: 로그인 페이지 컴포넌트 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import ProviderRoutes from './ProviderRoutes';
 import UserRoutes from './UserRoutes';
-// import { ContextProvider } from './contexts/Context';
 import './App.scss';
 import { LoginPage } from './pages';
 
-/*
 const Which = () => {
   return (
     <>
@@ -16,15 +14,15 @@ const Which = () => {
         <button>사용자 페이지!</button>
       </Link>
     </>
+<<<<<<< HEAD
   );
 };
-*/
 
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={LoginPage} /> //로그인 페이지로 변경
+        <Route exact path="/" component={Which} />
         <Route exact path="/pro" component={ProviderRoutes} />
         <Route exact path="/use" component={UserRoutes} />
       </Switch>
--- frontend/src/util/Modal.tsx
+++ frontend/src/util/Modal.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 type ModalType = {
   isOpen : boolean,
   close : () => void,
