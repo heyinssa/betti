5a773ad5a31823c689c06960564d7acc2a71b405
chichoon <chichoon.choi@gmail.com>
2021-12-07 16:04:44 +0900

    Feat: DefaultLoginForm 컴포넌트에서 로그인 핸들러 추가

--- frontend/src/components/LoginForms/DefaultLoginForm.tsx
+++ frontend/src/components/LoginForms/DefaultLoginForm.tsx
@@ -1,6 +1,22 @@
+import axios from 'axios';
+import passport from 'passport';
+
 const DefaultLoginForm: React.FC = () => {
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('id:', e.target.username.value, 'pw:', e.target.password.value);
+    axios.post(
+      '/login',
+      passport.authenticate('local', {
+        successRedirect: '/pro',
+        failureRedirect: '/',
+        failureFlash: true,
+      }),
+    );
+  };
+
   return (
-    <form action="/login" method="post">
+    <form onSubmit={handleLogin} method="post">
       <div>
         <label>Username : </label>
         <input type="text" name="username" />
