f136a3557049674f2a7388c511e702d76a2f4e31
chichoon <chichoon.choi@gmail.com>
2021-12-07 17:14:36 +0900

    Feat: App.tsx 충돌 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import ProviderRoutes from './ProviderRoutes';
 import UserRoutes from './UserRoutes';
+// import { ContextProvider } from './contexts/Context';
 import './App.scss';
 import { LoginPage } from './pages';
 
+/*
 const Which = () => {
   return (
     <>
@@ -16,12 +18,13 @@ const Which = () => {
     </>
   );
 };
+*/
 
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Which} />
+        <Route exact path="/" component={LoginPage} /> //로그인 페이지로 변경
         <Route exact path="/pro" component={ProviderRoutes} />
         <Route exact path="/use" component={UserRoutes} />
       </Switch>
