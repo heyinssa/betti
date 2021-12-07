e0662032f5504fb92d1ca5bff30caeb026626ca1
chichoon <chichoon.choi@gmail.com>
2021-12-08 00:51:34 +0900

    Feat: 프로젝트 이미지 컨테이너 컴포넌트 추가

---
+++ frontend/src/components/ProjectImageRow/ProjectImageRow.tsx
@@ -0,0 +1,16 @@
+import { Divider, Image } from "semantic-ui-react";
+
+const ProjectImageRow = () => {
+  return (
+    //좌우 스크롤 추가해야함
+    <Image.Group size="small">
+      <Image src="//이미지 위치" />
+      <Image src="//이미지 위치" />
+      <Image src="//이미지 위치" />
+      <Image src="//이미지 위치" />
+      <Image src="//이미지 위치" />
+    </Image.Group>
+  );
+};
+
+export default ProjectImageRow;
