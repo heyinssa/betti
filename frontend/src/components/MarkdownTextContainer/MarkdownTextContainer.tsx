a84b7051ef74393ad35ade8d047f5c0e021230fb
chichoon <chichoon.choi@gmail.com>
2021-12-08 00:39:06 +0900

    Feat: 마크다운 텍스트 영역 컴포넌트 추가

---
+++ frontend/src/components/MarkdownTextContainer/MarkdownTextContainer.tsx
@@ -0,0 +1,11 @@
+import { Container } from "semantic-ui-react";
+
+const MarkdownTextContainer = () => {
+  return (
+    <Container className="markdown-text-container">
+      <p>헐</p>
+    </Container>
+  );
+};
+
+export default MarkdownTextContainer;
