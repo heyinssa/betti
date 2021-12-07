d0334e6e18e0a8a5b24d67416e5fd6da8e8667f4
chichoon <chichoon.choi@gmail.com>
2021-12-08 00:52:07 +0900

    Feat: 페이지 라우트 추가

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,13 +1,18 @@
-import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  TesterMainScreen,
+  TesterProfileScreen,
+  TesterServiceDetailScreen,
+} from "./pages";
 
 const Rounter = () => {
   return (
     <Router>
       <Switch>
-        {
-          //Pages
-        }
+        <Route exact path="/profile" component={TesterProfileScreen} />
+        <Route exact path="/use" component={TesterMainScreen} />
+        <Route exact path="/use/test1" component={TesterServiceDetailScreen} />
+        <Route exact path="/use/Test2" component={TesterServiceDetailScreen} />
       </Switch>
     </Router>
   );
