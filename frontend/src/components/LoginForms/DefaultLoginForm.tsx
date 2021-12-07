6a5d394db780f0dcce293feba8b9505181d869a4
chichoon <chichoon.choi@gmail.com>
2021-12-07 14:46:20 +0900

    Feat: 로그인 폼 컴포넌트 추가

---
+++ frontend/src/components/LoginForms/DefaultLoginForm.tsx
@@ -0,0 +1,19 @@
+const DefaultLoginForm = () => {
+  return (
+    <form action="/login" method="post">
+      <div>
+        <label>Username : </label>
+        <input type="text" name="username" />
+      </div>
+      <div>
+        <label>Password : </label>
+        <input type="password" name="password" />
+      </div>
+      <div>
+        <input type="submit" value="로그인" />
+      </div>
+    </form>
+  );
+};
+
+export default DefaultLoginForm;
---
+++ frontend/src/components/LoginForms/LoginForms.tsx
@@ -0,0 +1,11 @@
+import DefaultLoginForm from './DefaultLoginForm';
+
+const LoginForms = () => {
+  return (
+    <div className="login-forms">
+      <DefaultLoginForm />
+    </div>
+  );
+};
+
+export default LoginForms;
