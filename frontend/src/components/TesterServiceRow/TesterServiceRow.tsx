496b4ec7056364a740fcb9e970e5f2a8a0f2a50a
chichoon <chichoon.choi@gmail.com>
2021-12-08 01:25:53 +0900

    Design: 프로젝트 컨테이너에 간단하게 css 넣어 가로방향 flex

--- frontend/src/components/TesterServiceRow/TesterServiceRow.tsx
+++ frontend/src/components/TesterServiceRow/TesterServiceRow.tsx
@@ -1,5 +1,6 @@
 import TesterServiceContainer from "./TesterServiceContainer";
 import { Header } from "semantic-ui-react";
+import "./TesterServiceRow.scss";
 
 const TesterServiceRow = () => {
   return (
@@ -8,6 +9,7 @@ const TesterServiceRow = () => {
         {
           //컨테이너 타이틀
         }
+        컨테이너 타이틀 임시
       </Header>
       <div className="tester-service-row-box">
         <TesterServiceContainer />
