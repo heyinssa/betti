f77cb2b67278a3cbabd4eeb90a273904cf598ed6
chichoon <chichoon.choi@gmail.com>
2021-12-08 01:25:33 +0900

    Rename: 페이지 index.jsx 타입스크립트로 변경

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,12 +1,10 @@
 import TesterMainScreen from "./TesterMainScreen";
-import TesterLoginScreen from "./TesterLoginScreen";
 import TesterProfileScreen from "./TesterProfileScreen";
 import TesterServiceDetailScreen from "./TesterServiceDetailScreen";
 import TesterWriteFeedbackScreen from "./TesterWriteFeedbackScreen";
 
 export {
   TesterMainScreen,
-  TesterLoginScreen,
   TesterProfileScreen,
   TesterServiceDetailScreen,
   TesterWriteFeedbackScreen,
