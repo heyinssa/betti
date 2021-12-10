b5797b3c5fa9bb20f9a3c110dd0d9e42b5aba1e3
Sangjoon Ham <gulsamcono@gmail.com>
2021-12-10 18:12:35 +0900

    Modify : 리듀서 주석 추가.

--- frontend/src/modules/Provider.ts
+++ frontend/src/modules/Provider.ts
@@ -1,7 +1,12 @@
+// 액션 타입 선언
+
 const CHANGE_TEAM = 'Provider/CHANGE_TEAM' as const;
 const CHANGE_TEST = 'Provider/CHANGE_TEST' as const;
 const ADD_TEAM = 'Provider/ADD_TEAM' as const;
 const ADD_TEST = 'Provider/ADD_TEST' as const;
+
+
+// 액션 생성 함수 선언
 
 export const changeTeam = (dst: number) => ({
     type: CHANGE_TEAM,
@@ -23,11 +28,18 @@ export const addTest = (curTeam: number, curTestArray: string[], newTestName: st
         curTeam, newTestName, curTestArray
     }
 });
+
+
+// 액션 객체들에 대한 타입 설정
+
 export type curStateAction =
     | ReturnType<typeof changeTeam>
     | ReturnType<typeof changeTest>
     | ReturnType<typeof addTeam>
     | ReturnType<typeof addTest>
+
+
+// 상태의 타입 및 초깃값 선언
 
 export type teamDataType = {
 
@@ -56,6 +68,9 @@ const initalState: StateType = {
     ],
 };
 
+
+// 리듀서 선언
+
 const Provider = (state: StateType = initalState, action: curStateAction) => {
     const stateData = state.stateData;
     const teamData = state.teamData;
@@ -70,7 +85,7 @@ const Provider = (state: StateType = initalState, action: curStateAction) => {
         }
         case ADD_TEST: {
             console.log('실행됨!');
-            const curTeamIndex = action.payload.curTeam; // state에서 끌어다 써도 될 듯
+            const curTeamIndex = action.payload.curTeam;
             const testName = action.payload.newTestName;
             const temp = [...action.payload.curTestArray, testName];
             teamData[curTeamIndex].test = temp;
