657ff92e44c8fe7154e3888d45b72fe43ef4ccfe
chichoon <chichoon.choi@gmail.com>
2021-12-11 23:22:57 +0900

    Design: 테스터 서비스 목록창 css 적용 (간략히)

--- frontend/src/components/TesterServiceRow/TesterServiceContainer.tsx
+++ frontend/src/components/TesterServiceRow/TesterServiceContainer.tsx
@@ -2,9 +2,9 @@ import { Card, Icon, Image } from "semantic-ui-react";
 
 const TesterServiceContainer = () => {
   return (
-    <Card className="tester-service-container">
+    <Card className="tester-service-container" href="/use/test1">
       <Image
-        src="https://emoji.slack-edge.com/T039P7U66/captain_taeng/5ac2bf3abd5e3d44.png"
+        src="https://avatars.githubusercontent.com/u/37893979?v=4"
         wrapped
         ui={false}
       />
@@ -27,7 +27,7 @@ const TesterServiceContainer = () => {
           {
             //프로젝트 소개
           }
-          간략한 팀 소개 입니다 ~~~~
+          간략한 팀 소개 입니다 ~~~~asdsadasdasdasdasdas asd adasdasdadasdasdas
         </Card.Description>
       </Card.Content>
       <Card.Content extra>
@@ -36,7 +36,7 @@ const TesterServiceContainer = () => {
             //클릭하면 본인 즐겨찾기 등록되도록 설정 (하트 채워짐 / 안채워짐)
           }
           <Icon name="heart" />
-          4242
+          123213
         </a>
       </Card.Content>
     </Card>
