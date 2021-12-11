d9fab1896972d8f71c0d20a31e8e59180b97d819
chichoon <chichoon.choi@gmail.com>
2021-12-12 00:50:56 +0900

    Design: css 크기 미세조정, 프로젝트 상세페이지 이미지 임시추가

--- frontend/src/components/TitleBar/TitleLogo.tsx
+++ frontend/src/components/TitleBar/TitleLogo.tsx
@@ -1,7 +1,7 @@
 const TitleLogo = () => {
   return (
     <div className="title-logo titlebar-icon">
-      <a href="//메인 페이지">
+      <a href="/use">
         {/* betti 아이콘으로 교체 */}
         <i className="home icon"></i>
       </a>
