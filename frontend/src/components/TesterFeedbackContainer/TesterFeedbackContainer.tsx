66b90d7bca10bc1ca7c217a3c484b80ad0cd7058
chichoon <chichoon.choi@gmail.com>
2021-12-12 17:47:38 +0900

    Feat: 별점 값 FeedbackStars 컴포넌트에서 받아오도록 추가

--- frontend/src/components/TesterFeedbackContainer/TesterFeedbackContainer.tsx
+++ frontend/src/components/TesterFeedbackContainer/TesterFeedbackContainer.tsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import TesterFeedbackStars from "./TesterFeedbackStars";
 import "./TesterFeedbackContainer.scss";
 
 const TesterFeedbackContainer = () => {
+  const [score, setScore] = useState(0);
+  const getFeedbackStars = (score: number) => {
+    console.log(score);
+    setScore(score);
+  };
+
   return (
     <div className="tester-feedback-container">
       <h1>피드백 남기기</h1>
       <form>
         <h2>파일 업로드</h2>
         <input type="file" accept="image/*" />
-        <TesterFeedbackStars />
+        <TesterFeedbackStars getScoreFunc={getFeedbackStars} /> {String(score)}
+        점
       </form>
     </div>
   );
