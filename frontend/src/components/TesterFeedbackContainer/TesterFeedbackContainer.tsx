cb87504b034306c903fcf8db9d22605c60cfdd7a
chichoon <chichoon.choi@gmail.com>
2021-12-12 17:29:41 +0900

    Feat: 테스터 피드백 페이지에 별점 추가

--- frontend/src/components/TesterFeedbackContainer/TesterFeedbackContainer.tsx
+++ frontend/src/components/TesterFeedbackContainer/TesterFeedbackContainer.tsx
@@ -1,5 +1,17 @@
+import TesterFeedbackStars from "./TesterFeedbackStars";
+import "./TesterFeedbackContainer.scss";
+
 const TesterFeedbackContainer = () => {
-  return <div>Container Test</div>;
+  return (
+    <div className="tester-feedback-container">
+      <h1>피드백 남기기</h1>
+      <form>
+        <h2>파일 업로드</h2>
+        <input type="file" accept="image/*" />
+        <TesterFeedbackStars />
+      </form>
+    </div>
+  );
 };
 
 export default TesterFeedbackContainer;
---
+++ frontend/src/components/TesterFeedbackContainer/TesterFeedbackStars.tsx
@@ -0,0 +1,16 @@
+const TesterFeedbackStars = () => {
+  return (
+    <div className="tester-feedback-stars">
+      <h2>별점</h2>
+      <div>
+        <i className="star icon"></i>
+        <i className="star icon"></i>
+        <i className="star icon"></i>
+        <i className="star icon"></i>
+        <i className="star icon"></i>
+      </div>
+    </div>
+  );
+};
+
+export default TesterFeedbackStars;
