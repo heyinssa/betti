a770450bfb42253bbfdbe4ee1a5147dc6a368579
chichoon <chichoon.choi@gmail.com>
2021-12-13 14:11:27 +0900

    Design: 타이틀바 임시 프로필사진 하드코딩

--- frontend/src/components/TitleBar/TitleBarProfileIcon.tsx
+++ frontend/src/components/TitleBar/TitleBarProfileIcon.tsx
@@ -2,7 +2,7 @@ const TitleBarProfileIcon = () => {
   return (
     <div className="titlebar-icon">
       <a href="/profile">
-        <img src="//asd" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/200px-React-icon.svg.png" />
       </a>
     </div>
   );
