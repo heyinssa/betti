4a17dc247667a7420dafe7916e168fa69ec28842
chichoon <chichoon.choi@gmail.com>
2021-12-13 13:48:52 +0900

    Feat: 프로젝트 데이터 담긴 타입 내용물 추가

--- frontend/src/modules/Provider.ts
+++ frontend/src/modules/Provider.ts
@@ -1,8 +1,20 @@
+export type TeammatesType = {
+  name: string;
+  profileImage: string;
+};
+
 export type ServiceType = {
   projectName: string;
+  projectImage: string;
   teamName: string;
+  teamImage: string;
+  teammates: Array<TeammatesType>;
   favourites: number;
   description: string;
   link: string;
-  img: string;
+  detailImage: Array<string>;
+};
+
+export type ServiceDataType = {
+  serviceData: ServiceType;
 };
