61c2604786881e205f27d3df7f1bec1020eec772
chichoon <chichoon.choi@gmail.com>
2021-12-28 01:54:22 +0900

    feat: 모달창 테스트

--- frontend/src/components/ProjectDetailHeader/ProjectButtonContainer.tsx
+++ frontend/src/components/ProjectDetailHeader/ProjectButtonContainer.tsx
@@ -1,10 +1,37 @@
-import { Button, Icon, Label } from "semantic-ui-react";
+import { useEffect, useState } from 'react';
+import { Button, Icon, Label } from 'semantic-ui-react';
+import Modal from 'react-modal';
 
 const ProjectButtonContainer = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClickOpenModal = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    e.preventDefault();
+    setIsOpen(true);
+    console.log(isOpen);
+  };
+  const handleClickCloseModal = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    e.preventDefault();
+    setIsOpen(false);
+    console.log(isOpen);
+  };
+  useEffect(() => {
+    Modal.setAppElement('body');
+  }, []);
   return (
     <div className="project-button-container">
-      <Button positive className="project-button">
+      <Button
+        positive
+        className="project-button"
+        onClick={handleClickOpenModal}
+      >
         베타테스트 신청
+        <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)}>
+          hi //내용채울곳 <button onClick={handleClickCloseModal}>닫기</button>
+        </Modal>
       </Button>
       <Button as="div" labelPosition="right" className="project-button">
         <Button icon>
