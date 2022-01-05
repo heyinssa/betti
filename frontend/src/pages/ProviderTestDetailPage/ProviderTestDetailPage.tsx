1849ef01e1dd804810f09a6fc14845fe0a9ff561
GulSam00 <gulsamcono@gmail.com>
2022-01-05 18:09:21 +0900

    Feat : pro/a/a 같이 문자로 들어오는 url 예외 처리. 존재하지 않는 숫자는 여전히 라우팅.

--- frontend/src/pages/ProviderTestDetailPage/ProviderTestDetailPage.tsx
+++ frontend/src/pages/ProviderTestDetailPage/ProviderTestDetailPage.tsx
@@ -1,12 +1,22 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../modules';
 
 const ProviderTestDetailPage = () => {
-  // const location = useLocation();
-  // const curPath = location.pathname;
-  // const parseTeam = curPath.substr(5, 1);
-  // const parseTest = curPath.substr(7, 1);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const curPath = location.pathname;
+  const parseTeam = curPath.substr(5, 1);
+  const parseTest = curPath.substr(7, 1);
+  console.log(parseInt(parseTeam))
+  console.log(isNaN(parseInt(parseTeam)));
+  if (isNaN(parseInt(parseTeam)) || isNaN(parseInt(parseTest))) {
+    navigate('/error');
+  }
+  // 와일드카드로 url을 받기에 형식이 같으면 전부 유효한 페이지로 인식하고 라우팅됨. 
+  // 문자일 경우는 parseInt로 구분 가능.
+  // 숫자일 경우에는 해당 숫자가 존재하는지, 하나하나 대조해봐야함.
+  // 테스트를 한 자리 수 (9개)까지 만들 수 있게 제한하면 pathname의 길이로 파싱이 가능해짐.
   const state = useSelector((state: RootState) => state.Provider);
   const { curTeam, curTest } = state.stateData;
   const teamData = state.teamData;
